878630be5773dc46140b49e3c980848488600c7f
Darcy <289904316@qq.com>
2024-05-20 20:54:55 +0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,9 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://globaldarcy.github.io/nine/',
-    // base: '/nine',
-    // trailingSlash: "ignore",
+    // site: 'https://globaldarcy.github.io/nine/',
+    base: '/nine',
+    trailingSlash: "ignore",
     server: {
         host: true,
         open: "/"
