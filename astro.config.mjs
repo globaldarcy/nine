d0f6018608b4ac49926f131085007d5783549f4e
Darcy <289904316@qq.com>
2024-05-20 20:33:43 +0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     base: '/',
-    trailingSlash: "never",
+    trailingSlash: "ignore",
     server: {
         host: true,
         open: "/"
